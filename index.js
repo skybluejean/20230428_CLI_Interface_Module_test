62bf0c6848a4adbf56200618770c9b7eae4e41d9
jin <ellenjin51@gmail.com>
2023-04-28 11:08:46 +0900

    create test file of inquirer

---
+++ index.js
@@ -0,0 +1,40 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      inputt : 'input',
+      name : 'input_type',
+      message : 'What is your name?',
+      default : 'Ellen'
+    },
+    {
+      type : 'list',
+      name : 'list question',
+      message : 'Who is your favorite character in Marvel Comics?',
+      choices : ['Ironman', 'Captain America', 'Thor', 'Spiderman'],
+      default : 'Hulk'
+    },
+    {
+      type : 'checkbox',
+      name : 'checkbox question',
+      message : 'Who is your favorite character in DC Comics?',
+      choices : ['Superman', 'Batman', 'Wonderwoman', 'Flash'],
+      default : 'Aquaman'
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  });
+
+  /*
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+  */
